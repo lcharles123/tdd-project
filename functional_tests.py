2f63a54528d342c1d4e5ad407500a907f37f9c4b
lcharles123 <tidus098@hotmail.com>
2022-05-22 00:31:41 +0100

    Refatorando o Teste Funcional

--- functional_tests.py
+++ functional_tests.py
@@ -12,7 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):  
         self.browser.quit()
-    
+        
+    def check_for_row_in_list_table(self, row_text):
+       table = self.browser.find_element_by_id('id_list_table')
+       rows = table.find_elements_by_tag_name('tr')
+       self.assertIn(row_text, [row.text for row in rows])
+       
     def test_can_start_a_list_and_retrieve_it_later(self): 
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
         self.browser.get('http://localhost:8000')
@@ -33,10 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Estudar testes funcionais" como um item da lista TODO
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
         # Ainda existe uma caixa de texto convidando para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
         inputbox = self.browser.find_element_by_id('id_new_item')  
@@ -44,17 +47,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         # A página atualiza novamente, e agora mostra ambos os itens na sua lista
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
-        self.assertIn('2: Estudar testes de unidade', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+
         # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
         # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
 
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
-
+    
 
 if __name__ == '__main__':
     unittest.main()
