60c0f5884731142eda3fb63fde98e5ee0e177e17
lcharles123 <tidus098@hotmail.com>
2022-05-21 18:15:49 +0100

    Melhorando a classe de teste com setUp e tearDown

--- functional_tests.py
+++ functional_tests.py
@@ -3,16 +3,20 @@ import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-    def test_can_start_a_list_and_retrieve_it_later(self): 
+    def setUp(self):
         opts = webdriver.FirefoxOptions()
         opts.add_argument("--headless") # porque estou executando em um servidor sem GUI
-        browser = webdriver.Firefox(options=opts)
-
+        self.browser = webdriver.Firefox(options=opts)
+    
+    def tearDown(self):  
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self): 
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
 
         # Ela nota que o título da página menciona TODO
-        self.assertIn('To-Do', browser.title)
+        self.assertIn('To-Do', self.browser.title)
 
         # Ela é convidada a entrar com um item TODO imediatamente
 
@@ -33,7 +37,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfeita, ela vai dormir
 
-        browser.quit()
 
 if __name__ == '__main__':
     unittest.main()
