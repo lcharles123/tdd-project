e936310a922821a60ff79cae3b058f2d1e38b177
lcharles123 <tidus098@hotmail.com>
2022-05-22 17:16:22 +0100

    Implementando o Novo Projeto: Ajustando o Modelo

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+
+class List(models.Model):
+    pass
+
+
 # class -> Tables
 # atributos -> columns
 # instancias -> linhas
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
+
+
