c7984e9609081938412eacdf06f59baee979ab08
lcharles123 <tidus098@hotmail.com>
2022-05-22 02:39:54 +0100

    Isolando Testes Funcionais

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
-import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         opts = webdriver.FirefoxOptions()
         opts.add_argument("--headless") # porque estou executando em um servidor sem GUI
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
        
     def test_can_start_a_list_and_retrieve_it_later(self): 
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
@@ -57,5 +57,3 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfeita, ela vai dormir
     
 
-if __name__ == '__main__':
-    unittest.main()
