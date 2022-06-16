2cb56b0721ecfc961ccfb4163811c07498d34c1a
lcharles123 <tidus098@hotmail.com>
2022-06-16 04:14:51 +0100

    De volta ao teste funcional - PARTE 1

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,7 +3,6 @@ from unittest import skip
 from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
-
     def test_cannot_add_empty_list_items(self):
 
         # Edith vai na home page e acidentalmente submete um item vazio
